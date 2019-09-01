4c4f2f7d91806fa2d447211adcfd67076aae5a04
imshivamdwivedi <dwivedishivam758@gmail.com>
2019-09-01 11:54:38 +0530

    Bear and Raspberry & Lunch Rush

---
+++ Problem385A.java
@@ -0,0 +1,30 @@
+    //============================================================================
+    //problem link: https://codeforces.com/problemset/problem/330/A
+    // Name        : Problem330A.java
+    // Author      : Shivam Dwivedi
+    // Version     :
+    // Copyright   : use it under your responsibility
+    //status:accepted
+    //============================================================================
+    import java.util.*;
+    import java.io.*;
+    import java.util.Scanner;
+     
+     
+    public class Problem385A {
+     
+        public static void main(String[] args){
+         
+              Scanner sc =  new Scanner(System.in);
+              int n = sc.nextInt();
+              int c = sc.nextInt(),ans=0;
+              int[] a = new int[n];
+              for(int i=0;i<n;i++){
+                a[i] = sc.nextInt();
+              }
+              for(int i=0;i<n-1;i++){
+                 ans = Math.max(ans,a[i]-a[i+1]-c);
+               }
+             System.out.println(ans);
+        }
+    }
