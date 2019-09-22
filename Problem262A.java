a974a91b82c9f634f52fb714581225b2df7d90c1
imshivamdwivedi <dwivedishivam758@gmail.com>
2019-09-22 14:28:24 +0530

    Roma and Lucky Numbers

--- Problem262A.java
+++ Problem262A.java
@@ -1,9 +1,36 @@
     import java.util.*;
     public class Problem262A {
+        
+        static int nod(int t){
+            if(t==0){
+                return 0;
+            }
+            else if(t>=1 && t<9){
+                return 1;
+            }
+            else{
+                return 1+nod(t/10);
+            }
+        }
      
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int n = sc.nextInt();
-         
+            int n = sc.nextInt(),b=n,ans=0;
+            int k = sc.nextInt();
+            while(n-->0){
+                int t = sc.nextInt(),count=0,r=0;
+                int nd = nod(t); 
+                while(nd-->0){
+                    r = t%10;
+                    if(r==4||r==7){
+                        count++;
+                    }
+                    t= t/10;
+                }
+                if(count>k){
+                    ans +=1;
+                }
+            }
+         System.out.println((b-ans));
         }
     }
